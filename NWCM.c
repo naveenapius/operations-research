af5894dcad06e5b80f3394fc589e0d4ebee8afc5
naveenapius <naveenapius@protonmail.com>
2023-08-16 01:59:52 +0530

    init NWCM

---
+++ NWCM.c
@@ -0,0 +1,57 @@
+/*
+North West Corner Method of obtaining initial basic feasible solution for transportation problems
+*/
+
+#include <stdio.h>
+#define SRCS 3   //number of sources
+#define DSTS 5  //number of destinations
+
+int srcs[SRCS], dsts[DSTS], cm[SRCS][DSTS];
+
+
+
+int initDests()
+{
+    //initialising global array DSTS
+    return 0;
+}
+
+
+int initSrcs()
+{
+    //initialising global array SRCS
+    return 0;
+}
+
+int initMatrix()
+{
+    //initialising cost matrix
+    return 0;
+}
+
+int checkBalance()
+{
+    int balance;
+    return balance;
+}
+
+
+int NWCM()
+{
+    //running algorithm to obtain initial basic feasible solution
+    int cost;
+    return cost;
+}
+
+
+
+
+
+
+
+//MAIN
+int main()
+{
+    //pass
+    
+}
