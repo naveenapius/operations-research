e535f6de69a1630b8537ed61047c0d91cd74f112
naveenapius <naveenapius@protonmail.com>
2023-08-16 22:12:36 +0530

    nwcm matrix init

--- NWCM.c
+++ NWCM.c
@@ -9,7 +9,7 @@ North West Corner Method of obtaining initial basic feasible solution for transp
 
 
 
-int dsts[DSTS], srcs[SRCS], cm[SRCS][DSTS];
+int dem[DSTS], sup[SRCS], cm[SRCS][DSTS];
 
 
 
@@ -20,7 +20,7 @@ int initDests()
     for(i=0; i<DSTS; i++)
     {
         printf("\nEnter demand at destination %d: ", i);
-        scanf("%d", &dsts[i]);
+        scanf("%d", &dem[i]);
     }
     printf("\nInitialised demands at all destinations");
     return 0;
@@ -33,17 +33,56 @@ int initSrcs()
     int i;
     for(i=0; i<SRCS; i++ )
     {
-        printf("\nEnter supply at source %d: ". i);
-        scanf("%d", &srcs[i]);
+        printf("\nEnter supply at source %d: ", i);
+        scanf("%d", &sup[i]);
     }
-    printf("\nInitialised supply at all sources: ");
+    printf("\nInitialised supply at all sources");
     return 0;
 }
 
 int initMatrix()
 {
-    //initialising cost matrix
+    int i,j;
+    for(i=0; i<SRCS; i++)
+    {
+        for(j=0; j<DSTS; j++)
+        {
+            printf("\nEnter cost of transport from src %d to dest %d: ", i,j);
+            scanf("%d", &cm[i][j]);
+        }
+    }
+    printf("\nInitialised cost matrix");
     return 0;
+}
+
+int showState()
+{
+    //display supply
+    int s;
+    printf("\n\nSupply:");
+    for(s=0; s<SRCS; s++)
+    {
+        printf("\nSource %d: %d", s, sup[s]);
+    }
+    //display demand
+    int d;
+    printf("\n\nDemand: ");
+    for(d=0; d<DSTS; d++)
+    {
+        printf("\nDestination %d: %d", d, dem[d]);
+    }
+    //display cost matrix
+    printf("\n\nCurrent cost matrix: ");
+    int i,j;
+    printf("\n");
+    for(i=0; i<SRCS; i++)
+    {
+        for(j=0; j<DSTS; j++)
+        {
+            printf("%d\t", cm[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int checkBalance()
@@ -69,6 +108,14 @@ int NWCM()
 //MAIN
 int main()
 {
-    //pass
-    
+    //initialising supply and demand 
+    initSrcs();
+    initDests();
+
+    //initialising cost matrix
+    initMatrix();
+
+    //showing current state
+    showState();
+    return 0;
 }
